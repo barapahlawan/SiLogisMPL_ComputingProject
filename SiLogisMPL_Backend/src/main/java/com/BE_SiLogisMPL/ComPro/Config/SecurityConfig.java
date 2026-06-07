5980303906b5f567337345ecff091a5fb5eacb6a
Rosalina Siagian <rosalinasiagian@MacBook-Air-Rosalina.local>
2026-06-08 01:43:15 +0700

    Fix 403 error on validation and prevent DB wipe

--- SiLogisMPL_Backend/src/main/java/com/BE_SiLogisMPL/ComPro/Config/SecurityConfig.java
+++ SiLogisMPL_Backend/src/main/java/com/BE_SiLogisMPL/ComPro/Config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
                 .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**", "/login/**").permitAll()
+                        .requestMatchers("/auth/**", "/login/**", "/error").permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
